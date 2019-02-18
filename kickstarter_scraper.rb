2b4f98c3648e13f34ba0e41504fef96ff0bd0498
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-18 22:21:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
-# require libraries/modules here
+require 'nokogiri'# require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  projects = {}
+  
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
 end
